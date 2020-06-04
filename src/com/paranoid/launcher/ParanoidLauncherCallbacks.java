AOSPA/android_packages_apps_ParanoidLauncher

ParanoidLauncher: Google Feed integration
If Google Search is installed, the Google Feed can be found at
the -1 screen (which is the very left).
This feature is enabled by default.

Change-Id: I495196818699fd378cd60e6dd61b07a0ab951762

src/com/paranoid/launcher/ParanoidLauncherCallbacks.java
@@ -21,6 +21,10 @@ import android.os.Bundle;
 
 import com.android.launcher3.AppInfo;
 import com.android.launcher3.LauncherCallbacks;
+import com.android.launcher3.settings.SettingsActivity;
+import com.android.launcher3.Utilities;
+
+import com.google.android.libraries.gsa.launcherclient.LauncherClient;
 
 import java.io.FileDescriptor;
 import java.io.PrintWriter;
@@ -28,9 +32,16 @@ import java.util.ArrayList;
 
 public class ParanoidLauncherCallbacks implements LauncherCallbacks,
         SharedPreferences.OnSharedPreferenceChangeListener {
+    public static final String SEARCH_PACKAGE = "com.google.android.googlequicksearchbox";
 
     private final ParanoidLauncher mLauncher;
 
+    private OverlayCallbackImpl mOverlayCallbacks;
+    private LauncherClient mLauncherClient;
+
+    private boolean mStarted;
+    private boolean mResumed;
+    private boolean mAlreadyOnHome;
 
     public ParanoidLauncherCallbacks(ParanoidLauncher launcher) {
         mLauncher = launcher;
@@ -38,32 +49,50 @@ public class ParanoidLauncherCallbacks implements LauncherCallbacks,
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
-
+        SharedPreferences prefs = Utilities.getPrefs(mLauncher);
+        mOverlayCallbacks = new OverlayCallbackImpl(mLauncher);
+        mLauncherClient = new LauncherClient(mLauncher, mOverlayCallbacks, getClientOptions(prefs));
+        mOverlayCallbacks.setClient(mLauncherClient);
+        prefs.registerOnSharedPreferenceChangeListener(this);
     }
 
     @Override
     public void onResume() {
+        mResumed = true;
+        if (mStarted) {
+            mAlreadyOnHome = true;
+        }
 
+        mLauncherClient.onResume();
     }
 
     @Override
     public void onStart() {
-
+        mStarted = true;
+        mLauncherClient.onStart();
     }
 
     @Override
     public void onStop() {
+        mStarted = false;
+        if (!mResumed) {
+            mAlreadyOnHome = false;
+        }
 
+        mLauncherClient.onStop();
     }
 
     @Override
     public void onPause() {
-
+        mResumed = false;
+        mLauncherClient.onPause();
     }
 
     @Override
     public void onDestroy() {
+        mLauncherClient.onDestroy();
 
+        Utilities.getPrefs(mLauncher).unregisterOnSharedPreferenceChangeListener(this);
     }
 
     @Override
@@ -83,12 +112,12 @@ public class ParanoidLauncherCallbacks implements LauncherCallbacks,
 
     @Override
     public void onAttachedToWindow() {
-
+        mLauncherClient.onAttachedToWindow();
     }
 
     @Override
     public void onDetachedFromWindow() {
-
+        mLauncherClient.onDetachedFromWindow();
     }
 
     @Override
@@ -98,7 +127,7 @@ public class ParanoidLauncherCallbacks implements LauncherCallbacks,
 
     @Override
     public void onHomeIntent(boolean internalStateHandled) {
-
+        mLauncherClient.hideOverlay(mAlreadyOnHome);
     }
 
     @Override
@@ -123,6 +152,17 @@ public class ParanoidLauncherCallbacks implements LauncherCallbacks,
 
     @Override
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        if (SettingsActivity.KEY_MINUS_ONE.equals(key)) {
+            mLauncherClient.setClientOptions(getClientOptions(sharedPreferences));
+        }
+    }
 
+    private LauncherClient.ClientOptions getClientOptions(SharedPreferences prefs) {
+        boolean hasPackage = ParanoidUtils.hasPackageInstalled(mLauncher, SEARCH_PACKAGE);
+        boolean isEnabled = prefs.getBoolean(SettingsActivity.KEY_MINUS_ONE, true);
+        return new LauncherClient.ClientOptions(hasPackage && isEnabled,
+                true, /* enableHotword */
+                true /* enablePrewarming */
+        );
     }
 }

juanr100/packages_apps_Launcher3

src/com/paranoid/launcher/ParanoidLauncherCallbacks.java
@@ -21,6 +21,10 @@ import android.os.Bundle;
 
 import com.android.launcher3.AppInfo;
 import com.android.launcher3.LauncherCallbacks;
+import com.android.launcher3.settings.SettingsActivity;
+import com.android.launcher3.Utilities;
+
+import com.google.android.libraries.gsa.launcherclient.LauncherClient;
 
 import java.io.FileDescriptor;
 import java.io.PrintWriter;
@@ -28,9 +32,16 @@ import java.util.ArrayList;
 
 public class ParanoidLauncherCallbacks implements LauncherCallbacks,
         SharedPreferences.OnSharedPreferenceChangeListener {
+    public static final String SEARCH_PACKAGE = "com.google.android.googlequicksearchbox";
 
     private final ParanoidLauncher mLauncher;
 
+    private OverlayCallbackImpl mOverlayCallbacks;
+    private LauncherClient mLauncherClient;
+
+    private boolean mStarted;
+    private boolean mResumed;
+    private boolean mAlreadyOnHome;
 
     public ParanoidLauncherCallbacks(ParanoidLauncher launcher) {
         mLauncher = launcher;
@@ -38,32 +49,50 @@ public class ParanoidLauncherCallbacks implements LauncherCallbacks,
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
-
+        SharedPreferences prefs = Utilities.getPrefs(mLauncher);
+        mOverlayCallbacks = new OverlayCallbackImpl(mLauncher);
+        mLauncherClient = new LauncherClient(mLauncher, mOverlayCallbacks, getClientOptions(prefs));
+        mOverlayCallbacks.setClient(mLauncherClient);
+        prefs.registerOnSharedPreferenceChangeListener(this);
     }
 
     @Override
     public void onResume() {
+        mResumed = true;
+        if (mStarted) {
+            mAlreadyOnHome = true;
+        }
 
+        mLauncherClient.onResume();
     }
 
     @Override
     public void onStart() {
-
+        mStarted = true;
+        mLauncherClient.onStart();
     }
 
     @Override
     public void onStop() {
+        mStarted = false;
+        if (!mResumed) {
+            mAlreadyOnHome = false;
+        }
 
+        mLauncherClient.onStop();
     }
 
     @Override
     public void onPause() {
-
+        mResumed = false;
+        mLauncherClient.onPause();
     }
 
     @Override
     public void onDestroy() {
+        mLauncherClient.onDestroy();
 
+        Utilities.getPrefs(mLauncher).unregisterOnSharedPreferenceChangeListener(this);
     }
 
     @Override
@@ -83,12 +112,12 @@ public class ParanoidLauncherCallbacks implements LauncherCallbacks,
 
     @Override
     public void onAttachedToWindow() {
-
+        mLauncherClient.onAttachedToWindow();
     }
 
     @Override
     public void onDetachedFromWindow() {
-
+        mLauncherClient.onDetachedFromWindow();
     }
 
     @Override
@@ -98,7 +127,7 @@ public class ParanoidLauncherCallbacks implements LauncherCallbacks,
 
     @Override
     public void onHomeIntent(boolean internalStateHandled) {
-
+        mLauncherClient.hideOverlay(mAlreadyOnHome);
     }
 
     @Override
@@ -123,6 +152,17 @@ public class ParanoidLauncherCallbacks implements LauncherCallbacks,
 
     @Override
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        if (SettingsActivity.MINUS_ONE_KEY.equals(key)) {
+            mLauncherClient.setClientOptions(getClientOptions(sharedPreferences));
+        }
+    }
 
+    private LauncherClient.ClientOptions getClientOptions(SharedPreferences prefs) {
+        boolean hasPackage = ParanoidUtils.hasPackageInstalled(mLauncher, SEARCH_PACKAGE);
+        boolean isEnabled = prefs.getBoolean(SettingsActivity.MINUS_ONE_KEY, true);
+        return new LauncherClient.ClientOptions(hasPackage && isEnabled,
+                true, /* enableHotword */
+                true /* enablePrewarming */
+        );
     }
 }